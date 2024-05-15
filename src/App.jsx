b78167af8d0f7b3150f7cf40d6d844398a0bbaba
Hieunm <hieunm@bytetech.io>
2024-05-15 18:22:39 +0700

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { menuHeader } from "./mockData";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listMenu, setlistMenu] = React.useState(menuHeader);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full h-screen bg-gray-200">
+      {/* Header */}
+      <div
+        className=""
+        style={{
+          background:
+            "linear-gradient(4deg, rgba(0,212,255,1) 0%, rgba(12,12,177,1) 50%, rgba(8,0,150,1) 100%)",
+        }}
+      >
+        {/* logo */}
+        <img
+          src="https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2019/08/Website-logo-1.png"
+          alt=""
+          className="h-16"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {/* Header Bottom */}
+      <ul className="bg-gray-900 text-white flex items-center justify-center gap-3 text-sm py-4">
+        {listMenu.map((item) => (
+          <li
+            className="uppercase relative before:absolute before:size-4 before:top-0 before:bg-gray-200 before:rotate-45 before:left-1/2 before:-translate-x-1/2 before:translate-y-7"
+            key={item.id}
+          >
+            {item.title}
+          </li>
+        ))}
+      </ul>
+
+      <div className="bg-gray-500 container mx-auto mt-4">Content</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/mockData.js
@@ -0,0 +1,37 @@
+export const menuHeader = [
+  {
+    id: 0,
+    title: "tin chung",
+    isActive: false,
+  },
+  {
+    id: 1,
+    title: "Vòng bảng",
+    isActive: false,
+  },
+  {
+    id: 2,
+    title: "Vòng loại trực tiếp",
+    isActive: false,
+  },
+  {
+    id: 3,
+    title: "Bảng xếp hạng",
+    isActive: false,
+  },
+  {
+    id: 4,
+    title: "Đội thi đấu",
+    isActive: false,
+  },
+  {
+    id: 4,
+    title: "Thông kê",
+    isActive: false,
+  },
+  {
+    id: 5,
+    title: "Bình chọn",
+    isActive: false,
+  },
+];
